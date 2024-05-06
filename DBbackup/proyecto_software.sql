633ba88d44d030b6b713819514c1a9c6224d03aa
Wilson <wvasquezg1@miumg.edu.gt>
2024-05-06 14:53:39 -0600

    1
    1

--- DBbackup/proyecto_software.sql
+++ DBbackup/proyecto_software.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-04-2024 a las 16:10:35
+-- Tiempo de generación: 06-05-2024 a las 21:50:54
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 5.6.40
 
@@ -29,12 +29,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `actividad` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `nombre_actividad` varchar(30) DEFAULT NULL,
   `descripcion` text,
   `punteo` decimal(10,0) DEFAULT NULL,
-  `id_etapa` int(11) DEFAULT NULL
+  `id_etapa` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `actividad`
+--
+
+INSERT INTO `actividad` (`id`, `nombre_actividad`, `descripcion`, `punteo`, `id_etapa`, `id_usuario`) VALUES
+(1, 'recapp', 'ya fue', '11', 7, NULL),
+(2, 'recap2', 'hoy la pagaron', '15', 8, NULL),
+(3, 'book1', 'venganzaaa', '5', 7, NULL),
+(4, 'actividad1', 'user1', '5', 9, 1),
+(5, 'actividad prueba2', 'descripcion', '5', 10, 1);
 
 -- --------------------------------------------------------
 
@@ -43,7 +55,7 @@ CREATE TABLE `actividad` (
 --
 
 CREATE TABLE `actividad2` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `nota_actividad` decimal(10,0) DEFAULT NULL,
   `id_estudiantes` int(11) DEFAULT NULL,
   `id_actividad` int(11) DEFAULT NULL
@@ -56,11 +68,29 @@ CREATE TABLE `actividad2` (
 --
 
 CREATE TABLE `clase` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `grado` varchar(25) DEFAULT NULL,
   `seccion` char(2) DEFAULT NULL,
-  `fecha` date DEFAULT NULL
+  `fecha` date DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clase`
+--
+
+INSERT INTO `clase` (`id`, `grado`, `seccion`, `fecha`, `id_usuario`) VALUES
+(1, 'kinder', 'A', '2024-04-26', NULL),
+(2, 'Kinder', 'B', '2024-04-26', NULL),
+(5, 'primero primaria', 'A', '2024-05-03', 1),
+(6, 'segundo primaria', 'A', '2024-05-03', 1),
+(7, 'tercero primaria', 'A', '2024-05-03', 1),
+(8, 'perito contador 4to', 'A', '2024-05-03', 2),
+(9, 'perito contador 5to', 'A', '2024-05-03', 2),
+(10, 'perito contador 6to', 'B', '2024-05-03', 2),
+(11, 'primero basico', 'A', '2024-05-05', 1),
+(12, 'segundo basico', 'A', '2024-05-06', 1),
+(13, 'tercero basico', 'A', '2024-05-06', 1);
 
 -- --------------------------------------------------------
 
@@ -69,12 +99,28 @@ CREATE TABLE `clase` (
 --
 
 CREATE TABLE `estudiante` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `clave` smallint(6) DEFAULT NULL,
   `total_nota` int(11) DEFAULT NULL,
   `id_persona` int(11) DEFAULT NULL,
-  `id_clase` int(11) DEFAULT NULL
+  `id_clase` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `estudiante`
+--
+
+INSERT INTO `estudiante` (`id`, `clave`, `total_nota`, `id_persona`, `id_clase`, `id_usuario`) VALUES
+(1, 1, NULL, 10, 2, NULL),
+(3, 2, NULL, 8, 2, NULL),
+(4, 1, NULL, 11, 7, 1),
+(5, 2, NULL, 12, 7, 1),
+(6, 1, NULL, 13, 8, 2),
+(7, 2, NULL, 14, 8, 2),
+(8, 1, NULL, 15, 9, 2),
+(9, 1, NULL, 16, 10, 2),
+(10, 1, NULL, 20, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -83,9 +129,20 @@ CREATE TABLE `estudiante` (
 --
 
 CREATE TABLE `etapa` (
-  `id` int(11) NOT NULL,
-  `nombre_etapa` varchar(25) DEFAULT NULL
+  `id` int(253) NOT NULL,
+  `nombre_etapa` varchar(25) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `etapa`
+--
+
+INSERT INTO `etapa` (`id`, `nombre_etapa`, `id_usuario`) VALUES
+(7, 'bimestre 1', NULL),
+(8, 'bimestre 2', NULL),
+(9, 'etapa user 1', 1),
+(10, 'segundo semestre', 1);
 
 -- --------------------------------------------------------
 
@@ -94,7 +151,7 @@ CREATE TABLE `etapa` (
 --
 
 CREATE TABLE `examen` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `nombre_examen` varchar(25) DEFAULT NULL,
   `descripcion` text,
   `total_examen` smallint(6) DEFAULT NULL
@@ -107,7 +164,7 @@ CREATE TABLE `examen` (
 --
 
 CREATE TABLE `examen2` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `nota_examen` decimal(10,0) DEFAULT NULL,
   `id_estudiante` int(11) DEFAULT NULL,
   `id_examen` int(11) DEFAULT NULL
@@ -120,7 +177,7 @@ CREATE TABLE `examen2` (
 --
 
 CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `usuario` varchar(20) DEFAULT NULL,
   `id_rol` int(11) DEFAULT NULL,
   `id_personas` int(11) DEFAULT NULL,
@@ -132,10 +189,18 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `usuario`, `id_rol`, `id_personas`, `pass`) VALUES
-(1, 'Wilson', 1, 1,'1'),
-(2, 'Pepito', 1, 2, '1'),
-(3, 'Mario', 2, 3,'1'),
-(4, 'Push', 3, 4, '1');
+(1, 'Wilson', 1, 1, '1'),
+(2, 'Pepito', 2, 2, '1'),
+(3, 'Mario', 2, 3, '1'),
+(4, 'Push', 3, 4, '1'),
+(7, 'viajero', 2, 5, '1'),
+(8, 'goku', 3, 6, '1'),
+(9, 'elpapu', 2, 7, '1'),
+(10, 'elmeromero', 1, 8, '1'),
+(12, 'gohan', 3, 10, '1'),
+(13, 'sdf', 2, 17, '1'),
+(14, 'dos', 2, 18, '1'),
+(15, 'alum', 3, 19, '1');
 
 -- --------------------------------------------------------
 
@@ -144,7 +209,7 @@ INSERT INTO `login` (`id`, `usuario`, `id_rol`, `id_personas`, `pass`) VALUES
 --
 
 CREATE TABLE `modulos` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `tipo_modelo` varchar(25) DEFAULT NULL,
   `comentario` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -156,7 +221,7 @@ CREATE TABLE `modulos` (
 --
 
 CREATE TABLE `permisos` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `agregar` tinyint(1) DEFAULT NULL,
   `eliminar` tinyint(1) DEFAULT NULL,
   `modificar` tinyint(1) DEFAULT NULL,
@@ -173,7 +238,7 @@ CREATE TABLE `permisos` (
 --
 
 CREATE TABLE `persona` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `codigo_personal` int(11) DEFAULT NULL,
   `nombres` varchar(30) DEFAULT NULL,
   `apellidos` varchar(30) DEFAULT NULL,
@@ -189,9 +254,24 @@ CREATE TABLE `persona` (
 
 INSERT INTO `persona` (`id`, `codigo_personal`, `nombres`, `apellidos`, `direccion`, `correo`, `celular`, `id_puesto`) VALUES
 (1, 1025, 'Wilson Antonio', 'Vasquez Grijalva', 'Guatemala', 'wvasquezg1@miumg.edu.gt', '44775039', 1),
-(2, 1, 'Pepito', 'Lopez', 'Antigua', 'xlive123x@gmail.com', '11223344', 2),
+(2, 1, 'Pepito', 'Lopez', 'Antigua', 'xlive123x@gmail.com', '11223344', 4),
 (3, 100, 'Mario', 'Bros', 'Boca del monte', 'nintendo@gmail.com', '44332211', 4),
-(4, 754, 'Anastacio', 'Push', 'Amatitlan', 'correo@colegio.com', '00552244', 6);
+(4, 754, 'Anastacio', 'Push', 'Amatitlan', 'correo@colegio.com', '00552244', 6),
+(5, NULL, 'linux ze', 'Op', NULL, 'linux@gmail.com', NULL, 4),
+(6, NULL, 'pancho', 'panchito', NULL, 'panchon@gmail.com', NULL, 6),
+(7, NULL, 'elchavo', 'elchavin', NULL, 'elchavon@gmail.com', NULL, 4),
+(8, NULL, 'danonino', 'danonine', NULL, 'dano@gmail.com', NULL, 4),
+(10, NULL, 'papu', 'mamu', NULL, 'papu@gmail.com', NULL, 6),
+(11, NULL, 'wil', 'wils', NULL, 'f@gmail.com', NULL, 6),
+(12, NULL, 'prueba', 'pruebas', NULL, 'f@gmail.com', NULL, 6),
+(13, NULL, 'alumno de Pepito', 'Prueba2', NULL, 'profepepito@gmail.com', NULL, 6),
+(14, NULL, 'alumno de Pepito 2', 'prueba4', NULL, 'correoyoquese@gmail.com', NULL, 6),
+(15, NULL, 'alumno de pepito 3', 'ya se', NULL, 'ya_gane@gmail.con', NULL, 6),
+(16, NULL, 'prueba de fallo', 'prueba1', NULL, 'yase@gmail.com', NULL, 6),
+(17, NULL, 'fs', 'fs', NULL, 'f', NULL, 4),
+(18, NULL, 'dos', 'dos', NULL, 'dos', NULL, 4),
+(19, NULL, 'hola', 'ds', NULL, 'f', NULL, 6),
+(20, NULL, 'alumnoprueba', 'dos', NULL, 'tres', NULL, 6);
 
 -- --------------------------------------------------------
 
@@ -200,7 +280,7 @@ INSERT INTO `persona` (`id`, `codigo_personal`, `nombres`, `apellidos`, `direcci
 --
 
 CREATE TABLE `puesto` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `descripcion` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -224,7 +304,7 @@ INSERT INTO `puesto` (`id`, `descripcion`) VALUES
 --
 
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
+  `id` int(253) NOT NULL,
   `nombre` varchar(15) DEFAULT NULL,
   `descripcion` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- querys_repo/queryss.sql
+++ querys_repo/queryss.sql
@@ -129,17 +129,31 @@ select id,clave,total_nota,id_persona,id_clase from estudiante) t4 on t4.id_clas
 -------------------------------------------------------------------------------------------------------------------
 USUARIOS 
 /*tabla de usuarios*/
-    select 
-        t1.id,t1.usuario,t2.nombre as rol,t3.nombres,t3.apellidos,t3.correo,t4.descripcion, 'X' as acciones
-    from 
-    ( /*tabla login*/ 
-        select id,usuario,id_rol,id_personas,pass from login ) t1 left join 
-    (/*tabla roles*/ 
-        select id,nombre,descripcion from roles ) t2 on t1.id_rol = t2.id left JOIN 
-    (/*tabla persona*/ 
-        select id,nombres,apellidos,correo,id_puesto from persona ) t3 on t1.id_personas = t3.id left JOIN 
-    (/*tabla puesto*/ 
-        select id,descripcion from puesto ) t4 on t3.id_puesto = t4.id 
+	select 
+			t1.id,t1.usuario,t2.nombre as rol,t3.nombres,t3.apellidos,t3.correo,t4.descripcion, 'X' as acciones
+	FROM 
+			( /*tabla login*/ 
+					select id,usuario,id_rol,id_personas,pass from login ) t1 left join 
+			(/*tabla roles*/ 
+					select id,nombre,descripcion from roles ) t2 on t1.id_rol = t2.id left join 
+			(/*tabla persona*/ 
+					select id,nombres,apellidos,correo,id_puesto from persona ) t3 on t1.id_personas = t3.id left join 
+			(/*tabla puesto*/ 
+					select id,descripcion from puesto ) t4 on t3.id_puesto = t4.id 
+
+        start transaction;
+
+        -- Insertar en la tabla de persona
+        insert into persona (nombres, apellidos, correo, id_puesto)
+        values ('fs', 'fs', 'f', 4);
+
+        -- Insertar en la tabla de login
+        insert into login (usuario, id_rol, id_personas, pass)
+        values ('sdf', 2, 17 , '1');
+
+        commit;
+
+
 
 /*manda a traer el ultimo id*/				
 select id+1 as id from persona order by id desc limit 1
